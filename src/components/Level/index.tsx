5d4b3e3a721b3b67b56219b02b8c5e150cd48320
mateus <mateusweslley14@gmail.com>
2025-02-03 17:31:24 -0300

    adicionando código fonte do outro projeto

---
+++ src/components/Level/index.tsx
@@ -0,0 +1,61 @@
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+import { THEME } from "../../styles/theme";
+import { styles } from "./styles";
+
+const TYPE_COLORS = {
+  EASY: THEME.COLORS.BRAND_LIGHT,
+  HARD: THEME.COLORS.DANGER_LIGHT,
+  MEDIUM: THEME.COLORS.WARNING_LIGHT,
+};
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  isChecked?: boolean;
+  type?: keyof typeof TYPE_COLORS;
+};
+
+export function Level({
+  title,
+  type = "EASY",
+  isChecked = false,
+  ...rest
+}: Props) {
+  const scale = useSharedValue(1);
+
+  const animatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
+  const COLOR = TYPE_COLORS[type];
+
+  return (
+    <TouchableOpacity {...rest}>
+      <Animated.View
+        style={[
+          styles.container,
+          animatedContainerStyle,
+          {
+            borderColor: COLOR,
+            backgroundColor: isChecked ? COLOR : "transparent",
+          },
+        ]}
+      >
+        <Text
+          style={[
+            styles.title,
+            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR },
+          ]}
+        >
+          {title}
+        </Text>
+      </Animated.View>
+    </TouchableOpacity>
+  );
+}
