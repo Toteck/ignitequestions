baf1599b86efff47758c03299ee930efb1a162a3
mateus <mateusweslley14@gmail.com>
2025-02-25 11:52:28 -0300

    criando header para animar

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Alert, Easing, ScrollView, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 
 import Animated, {
   useAnimatedStyle,
@@ -7,6 +7,7 @@ import Animated, {
   withSequence,
   interpolate,
   withTiming,
+  useAnimatedScrollHandler,
 } from "react-native-reanimated";
 
 import { useNavigation, useRoute } from "@react-navigation/native";
@@ -21,6 +22,8 @@ import { Question } from "../../components/Question";
 import { QuizHeader } from "../../components/QuizHeader";
 import { ConfirmButton } from "../../components/ConfirmButton";
 import { OutlineButton } from "../../components/OutlineButton";
+import { ProgressBar } from "../../components/ProgressBar";
+import { THEME } from "../../styles/theme";
 
 interface Params {
   id: string;
@@ -38,6 +41,7 @@ export function Quiz() {
   );
 
   const shake = useSharedValue(0);
+  const scrollY = useSharedValue(0);
 
   const { navigate } = useNavigation();
 
@@ -122,6 +126,20 @@ export function Quiz() {
     };
   });
 
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const fixedProgressBarStyles = useAnimatedStyle(() => {
+    return {
+      position: "absolute",
+      paddingTop: 50,
+      backgroundColor: THEME.COLORS.GREY_500,
+      width: "110%",
+      left: "-5%",
+    };
+  });
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0];
     setQuiz(quizSelected);
@@ -140,9 +158,18 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <ScrollView
+      <Animated.View style={fixedProgressBarStyles}>
+        <Text style={styles.title}>{quiz.title}</Text>
+        <ProgressBar
+          total={quiz.questions.length}
+          current={currentQuestion + 1}
+        />
+      </Animated.View>
+      <Animated.ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.question}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
       >
         <QuizHeader
           title={quiz.title}
@@ -163,7 +190,7 @@ export function Quiz() {
           <OutlineButton title="Parar" onPress={handleStop} />
           <ConfirmButton onPress={handleConfirm} />
         </View>
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   );
 }
